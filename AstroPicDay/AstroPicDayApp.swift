78eb735ae0f45db0ad708205fe12f6671d71eeba
Mayank Gandhi <mayank@radiusagent.com>
2024-06-01 14:41:58 +0530

    init

---
+++ AstroPicDay/AstroPicDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstroPicDayApp.swift
+//  AstroPicDay
+//
+//  Created by Mayank Gandhi on 01/06/24.
+//
+
+import SwiftUI
+
+@main
+struct AstroPicDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
