2dfe5521fd3d988afa12769491574c806ccf93bd
Mayank Gandhi <mayank@radiusagent.com>
2024-06-02 21:12:41 +0530

    Picture List Unit tests

--- PictureList/Tests/PictureListTests/PictureListTests.swift
+++ PictureList/Tests/PictureListTests/PictureListTests.swift
@@ -1,12 +1,52 @@
 import XCTest
 @testable import PictureList
+import Utils
+import ComposableArchitecture
+import UtilsTests
 
+@MainActor
 final class PictureListTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
 
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    func testFetchPicturesOfTheWeek_Success() async throws {
+        
+        let mockData = """
+        [
+          {
+            "copyright": "\\nPhil Hart\\n",
+            "date": "2024-04-02",
+            "explanation": "Only in the fleeting darkness of a total solar eclipse is the light of the solar corona easily visible.",
+            "hdurl": "https://apod.nasa.gov/apod/image/2404/CoronaExmouth_Hart_1920.jpg",
+            "media_type": "image",
+            "service_version": "v1",
+            "title": "Detailed View of a Solar Eclipse Corona",
+            "url": "https://apod.nasa.gov/apod/image/2404/CoronaExmouth_Hart_1080.jpg"
+          }
+        ]
+        """.data(using: .utf8)!
+        
+        let requestController = MockRequestControllerWithSingularData(mockResponse: mockData)
+                
+        let store = TestStore(
+            initialState: PictureList.State(),
+            reducer: { PictureList() }
+        ) {
+            $0.requestController = requestController
+        }
+
+        XCTAssertEqual(store.state.viewState, .loading)
+        
+        await store.send(.fetchPicturesOfTheWeek)
+
+        await store.receive(.presentPicturesOfTheWeek([
+            PictureListItem.State(date: "2 Apr 2024", explanation: "Only in the fleeting darkness of a total solar eclipse is the light of the solar corona easily visible.", hdurl: URL(string: "https://apod.nasa.gov/apod/image/2404/CoronaExmouth_Hart_1920.jpg"), mediaType: "image", serviceVersion: "v1", title: "Detailed View of a Solar Eclipse Corona", url: URL(string: "https://apod.nasa.gov/apod/image/2404/CoronaExmouth_Hart_1080.jpg")!, copyright: "\nPhil Hart\n")
+        ])) {
+            $0.pictureListItems = IdentifiedArray(uniqueElements: [
+                PictureListItem.State(date: "2 Apr 2024", explanation: "Only in the fleeting darkness of a total solar eclipse is the light of the solar corona easily visible.", hdurl: URL(string: "https://apod.nasa.gov/apod/image/2404/CoronaExmouth_Hart_1920.jpg"), mediaType: "image", serviceVersion: "v1", title: "Detailed View of a Solar Eclipse Corona", url: URL(string: "https://apod.nasa.gov/apod/image/2404/CoronaExmouth_Hart_1080.jpg")!, copyright: "\nPhil Hart\n")
+            ])
+            
+            $0.viewState = .results
+        }
+        
     }
+
 }
